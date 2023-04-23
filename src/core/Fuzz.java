364fa035f41f2e7d040eb507929409c1c36f566f
Enan Ajmain <3nan.ajmain@gmail.com>
2023-04-23 10:46:10 +0600

    refactor(Fuzz): hardcode domains

--- src/core/Fuzz.java
+++ src/core/Fuzz.java
@@ -129,33 +129,23 @@ public class Fuzz {
      */
     private static ArrayList<String> getDomains(){
         ArrayList<String> domains=new ArrayList<String>();
-        boolean added=false;
-        try {
-            URL dURL=new URL("https://raw.githubusercontent.com/TwitchRecover/TwitchRecover/main/domains.txt");
-            HttpURLConnection con=(HttpURLConnection) dURL.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("User-Agent", "Mozilla/5.0");
-            if(con.getResponseCode()==HttpURLConnection.HTTP_OK){
-                BufferedReader br=new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String line = null;
-                while((line=br.readLine()) !=null){
-                    String response=line.toString();
-                    domains.add(response);
-                    added=true;
-                }
-            }
-        }
-        catch(IOException ignored){}
-        finally{
-            if(!added){     //To execute if the domains from the domains file were not added as a backup.
-                domains.add("https://vod-secure.twitch.tv");
-                domains.add("https://vod-metro.twitch.tv");
-                domains.add("https://d2e2de1etea730.cloudfront.net");
-                domains.add("https://dqrpb9wgowsf5.cloudfront.net");
-                domains.add("https://ds0h3roq6wcgc.cloudfront.net");
-                domains.add("https://dqrpb9wgowsf5.cloudfront.net");
-            }
-        }
+        domains.add("https://vod-secure.twitch.tv");
+        domains.add("https://vod-metro.twitch.tv");
+        domains.add("https://vod-pop-secure.twitch.tv");
+        domains.add("https://d2e2de1etea730.cloudfront.net");
+        domains.add("https://dqrpb9wgowsf5.cloudfront.net");
+        domains.add("https://ds0h3roq6wcgc.cloudfront.net");
+        domains.add("https://d2nvs31859zcd8.cloudfront.net");
+        domains.add("https://d2aba1wr3818hz.cloudfront.net");
+        domains.add("https://d3c27h4odz752x.cloudfront.net");
+        domains.add("https://dgeft87wbj63p.cloudfront.net");
+        domains.add("https://d1m7jfoe9zdc1j.cloudfront.net");
+        domains.add("https://d3vd9lfkzbru3h.cloudfront.net");
+        domains.add("https://d2vjef5jvl6bfs.cloudfront.net");
+        domains.add("https://d1ymi26ma8va5x.cloudfront.net");
+        domains.add("https://d1mhjrowxxagfy.cloudfront.net");
+        domains.add("https://ddacn6pr5v0tl.cloudfront.net");
+        domains.add("https://d3aqoihi2n8ty8.cloudfront.net");
         return domains;
     }
 
